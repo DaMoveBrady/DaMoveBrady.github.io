d437ef7480dc14320f7f3df94fcf46b5107b770a
andy04912 <andy04912@mail.fcu.edu.tw>
2020-04-01 17:11:34 +0800

    Updates

---
+++ precache-manifest.77c1c148540d8a67f8ebfcb408ecf887.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d332c6ea52f7db5a4b34b88cf46a93c",
+    "url": "/DaMoveBrady.github.io/index.html"
+  },
+  {
+    "revision": "f0bb9b1d18cef24fcc95",
+    "url": "/DaMoveBrady.github.io/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "d7792a12eab238143b4f",
+    "url": "/DaMoveBrady.github.io/static/js/2.58617b89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DaMoveBrady.github.io/static/js/2.58617b89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0bb9b1d18cef24fcc95",
+    "url": "/DaMoveBrady.github.io/static/js/main.f0f79a31.chunk.js"
+  },
+  {
+    "revision": "541f5fab525bb813c79c",
+    "url": "/DaMoveBrady.github.io/static/js/runtime-main.59fe22b1.js"
+  }
+]);
